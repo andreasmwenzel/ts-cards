6ccf3de0e5858e58445d91f263db91892490fa69
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2021-05-03 20:39:44 -0700

    added sorted cards list to trick and renames winner to winning card

--- src/trick.ts
+++ src/trick.ts
@@ -4,25 +4,30 @@ import { Suit } from './suit';
 export class Trick {
   private leadSuit;
   private trumpSuit;
-  public cards: Card[] = [];
+  private _cards: Card[] = [];
+  private sortedCards: Card[] = [];
   private compareCards: (a: Card, b: Card, ledSuit?: Suit, trump?: Suit) => number;
 
   constructor(card: Card, cardCompare: (a: Card, b: Card, leadSuit?: Suit, trump?: Suit) => number, trump?: Suit) {
     this.leadSuit = card.suit;
     this.trumpSuit = trump;
     this.compareCards = cardCompare;
-    this.cards.push(card);
+    this.addCard(card);
   }
 
   addCard(card: Card) {
     this.cards.push(card);
-    this.cards.sort((a: Card, b: Card) => {
+    this.sortedCards.push(card);
+    this.sortedCards.sort((a: Card, b: Card) => {
       return this.compareCards(a, b, this.leadSuit, this.trumpSuit);
     });
   }
 
-  public get winner() {
-    return this.cards[0];
+  public get cards(){
+    return this._cards;
+  }
+  public get winningCard() {
+    return this.sortedCards[0];
   }
   public get length() {
     return this.cards.length;
